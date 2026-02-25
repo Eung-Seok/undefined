c685ab3631bf10749b156e36030ef5a95d16849c
Eung-Seok <rwjflrk1234@gmail.com>
2026-02-25 10:18:02 +0900

    로그인 코드 수정

--- Project/src/main/java/com/app/controller/LoginController.java
+++ Project/src/main/java/com/app/controller/LoginController.java
@@ -11,13 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.dto.user.User;
+import com.app.service.role.RoleService;
 import com.app.service.user.UserService;
+import com.app.service.userRole.UserRoleService;
 
 @Controller
 public class LoginController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserRoleService userRoleService;
 
     // [1] 로그인 페이지
     @GetMapping("/login")
@@ -27,22 +33,9 @@ public class LoginController {
 
     // [2] 로그인 실행
     @PostMapping("/login")
-    public String loginProcess(User user, String saveId, HttpServletResponse response, HttpSession session, Model model) {
-        User dbUser = userService.findUserByEmpno(user.getEmpno());
+	public String loginProcess(User user, HttpSession session, Model model) {
 
-        if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
-            session.setAttribute("loginUser", dbUser);
-
-            // 1. 쿠키 생성 (사번 저장)
-            Cookie cookie = new Cookie("savedEmpno", String.valueOf(dbUser.getEmpno()));
-            cookie.setPath("/");
-
-            if (saveId != null) {
-                // 사원번호 저장 체크 시: 7일 유지
-                cookie.setMaxAge(60 * 60 * 24 * 7);
-            } else {     
-                cookie.setMaxAge(-1); // -1은 세션 쿠키로, 브라우저 종료 시 삭제됨
-            }
+		User dbUser = userService.findUserByEmpno(user.getEmpno());
 
 		if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
 			session.setAttribute("loginUser", dbUser);
@@ -50,6 +43,10 @@ public class LoginController {
 			return "redirect:/dashboard";
 		} else {
 
+			model.addAttribute("loginError", "사원번호 또는 비밀번호가 잘못되었습니다.");
+			return "login/login";
+		}
+	}
     //  회원가입 페이지 
     @GetMapping("/signup")
     public String signupPage() {
